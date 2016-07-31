4f38480b1668a3699241a969a610b3faa3f53e65
Vipul Jha <vipuljha08@gmail.com>
2016-07-31 16:47:27 +0530

    Whole build script changed
    The build script was originally written by Varun chitre. I just modified it according to my use.

--- build.sh
+++ build.sh
@@ -1,51 +1,62 @@
- #
- # Copyright © 2015, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
- #
- # Custom build script
- #
- # This software is licensed under the terms of the GNU General Public
- # License version 2, as published by the Free Software Foundation, and
- # may be copied, distributed, and modified under those terms.
- #
- # This program is distributed in the hope that it will be useful,
- # but WITHOUT ANY WARRANTY; without even the implied warranty of
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- # GNU General Public License for more details.
- #
- # Please maintain this if you use this script or any part of it
- #
+#
+# Copyright Â© 2016, "Vipul Jha" <vipuljha08@gmail.com>
+# Thanks to Kunal Kene "kunalkene1797" <kunalkene1797@gmail.com>
+# Thanks to Varun Chitre "varunchitre15" <varunchitre15@gmail.com>
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+
+# Inits
 KERNEL_DIR=$PWD
 ZIMAGE=$KERNEL_DIR/arch/arm/boot/zImage
 BUILD_START=$(date +"%s")
-blue='\033[0;34m'
-cyan='\033[0;36m'
-yellow='\033[0;33m'
-red='\033[0;31m'
-nocol='\033[0m'
-# Modify the following variable if you want to build
-export CROSS_COMPILE="/home/varunchitre15/linaro-5.2/bin/arm-eabi-"
+
+# Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+nocol='\033[0m'         # Default
+
+# Exports
 export ARCH=arm
 export SUBARCH=arm
-export KBUILD_BUILD_USER="varun.chitre15"
-export KBUILD_BUILD_HOST="EroticBox"
+export KBUILD_BUILD_USER="VipulJha"
+export KBUILD_BUILD_HOST="TechFlue"
+export CROSS_COMPILE="/home/vipul/arm-eabi-6.0/bin/arm-eabi-"
 
+# Compilation Scripts
 compile_kernel ()
 {
-echo -e "$blue***********************************************"
-echo "          Compiling ThunderZap kernel          "
+echo -e "$White***********************************************"
+echo "        Compiling ElectraBlue Kernel           "
 echo -e "***********************************************$nocol"
-make cyanogenmod_sprout_defconfig
-make -j12
+make clean && make mrproper
+make sprout_defconfig
+make -j2
 if ! [ -a $ZIMAGE ];
 then
-echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+echo -e "$Red Kernel Compilation failed! Please Fix the errors! $nocol"
 exit 1
 fi
 }
 
+# Finalizing Scripts
 case $1 in
 clean)
-make ARCH=arm -j8 clean mrproper
+make ARCH=arm -j2 clean mrproper
 rm -rf include/linux/autoconf.h
 ;;
 *)
@@ -54,4 +65,4 @@ compile_kernel
 esac
 BUILD_END=$(date +"%s")
 DIFF=$(($BUILD_END - $BUILD_START))
-echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+echo -e "$Yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
